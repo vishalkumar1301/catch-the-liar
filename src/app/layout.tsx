6d6dcf5d2367038ad599966ee5d37f848fa9139c
vishal kumar <vishalkumar1301@gmail.com>
2024-11-17 19:46:59 -0500

    Added Datafast tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,21 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5HBWL8MN');
-          `}
-        </Script>
+        <Script 
+          defer
+          data-website-id="673a8e58acf2c3130a5d35d9"
+          data-domain="catchtheliars.com"
+          src="https://datafa.st/js/script.js"
+        />
       </head>
       <body className={inter.className}>
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5HBWL8MN"
-            height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>
-        </noscript>
         <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
           <Header />
           <div className="container px-4 mx-auto">
